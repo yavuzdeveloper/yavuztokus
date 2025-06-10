442c623277ccf60b03b1aa7ed26e4aa7b65fe993
yavuztokus <yavuztokus2@gmail.com>
2025-06-10 19:21:48 +0100

    fixing path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  base: "/",
+  base: "/yavuztokus",
   build: {
     rollupOptions: {
       output: {
